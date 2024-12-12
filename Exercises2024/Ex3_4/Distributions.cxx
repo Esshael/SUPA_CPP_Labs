351e8e1b11b79af53c6911a80103eb16afe7d75d
Esshael <91078404+Esshael@users.noreply.github.com>
2024-12-12 11:19:05 +0000

    2

--- Exercises2024/Ex3_4/Distributions.cxx
+++ Exercises2024/Ex3_4/Distributions.cxx
@@ -1,107 +1,54 @@
 #include "Distributions.h"
-#include <iostream>
 #include <cmath>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
+#include <iostream>
 
-// Helper function for generating uniform random numbers
-double generateRandomUniform() {
-    return static_cast<double>(rand()) / RAND_MAX;
-}
-
-// Normal Distribution Implementation
-NormalDistribution::NormalDistribution(double mu, double sigma, double range_min, double range_max, std::string outfile)
-    : FiniteFunction(range_min, range_max, outfile), m_mu(mu), m_sigma(sigma) {}
+// Normal Distribution
+NormalDistribution::NormalDistribution(double mean, double sigma, double range_min, double range_max, std::string outfile)
+    : FiniteFunction(range_min, range_max, outfile), m_Mean(mean), m_Sigma(sigma) {}
 
 double NormalDistribution::callFunction(double x) {
-    double norm = 1.0 / (m_sigma * std::sqrt(2 * M_PI));
-    return norm * std::exp(-0.5 * std::pow((x - m_mu) / m_sigma, 2));
+    double norm_factor = 1.0 / (m_Sigma * sqrt(2 * M_PI));
+    double exponent = -0.5 * pow((x - m_Mean) / m_Sigma, 2);
+    return norm_factor * exp(exponent);
 }
 
 void NormalDistribution::printInfo() {
+    std::cout << "Normal Distribution with mean = " << m_Mean << ", sigma = " << m_Sigma << std::endl;
     FiniteFunction::printInfo();
-    std::cout << "Normal Distribution with mu = " << m_mu << ", sigma = " << m_sigma << std::endl;
 }
 
-std::vector<double> NormalDistribution::sampleData(int nPoints, double stepSize) {
-    std::vector<double> samples;
-    double x = m_RMin + (m_RMax - m_RMin) * generateRandomUniform(); // Start with a random point
-    for (int i = 0; i < nPoints; ++i) {
-        double y = x + stepSize * (generateRandomUniform() - 0.5); // Propose new point y
-        double A = std::min(callFunction(y) / callFunction(x), 1.0);
-        if (generateRandomUniform() < A) {
-            x = y; // Accept y
-        }
-        samples.push_back(x);
-    }
-    return samples;
-}
-
-// Cauchy-Lorentz Distribution Implementation
+// Cauchy-Lorentz Distribution
 CauchyLorentzDistribution::CauchyLorentzDistribution(double x0, double gamma, double range_min, double range_max, std::string outfile)
-    : FiniteFunction(range_min, range_max, outfile), m_x0(x0), m_gamma(gamma) {}
+    : FiniteFunction(range_min, range_max, outfile), m_X0(x0), m_Gamma(gamma) {}
 
 double CauchyLorentzDistribution::callFunction(double x) {
-    double norm = 1.0 / (M_PI * m_gamma);
-    return norm / (1 + std::pow((x - m_x0) / m_gamma, 2));
+    double norm_factor = 1.0 / (M_PI * m_Gamma);
+    double denominator = 1 + pow((x - m_X0) / m_Gamma, 2);
+    return norm_factor / denominator;
 }
 
 void CauchyLorentzDistribution::printInfo() {
+    std::cout << "Cauchy-Lorentz Distribution with x0 = " << m_X0 << ", gamma = " << m_Gamma << std::endl;
     FiniteFunction::printInfo();
-    std::cout << "Cauchy-Lorentz Distribution with x0 = " << m_x0 << ", gamma = " << m_gamma << std::endl;
 }
 
-std::vector<double> CauchyLorentzDistribution::sampleData(int nPoints, double stepSize) {
-    std::vector<double> samples;
-    double x = m_RMin + (m_RMax - m_RMin) * generateRandomUniform(); // Start with a random point
-    for (int i = 0; i < nPoints; ++i) {
-        double y = x + stepSize * (generateRandomUniform() - 0.5); // Propose new point y
-        double A = std::min(callFunction(y) / callFunction(x), 1.0);
-        if (generateRandomUniform() < A) {
-            x = y; // Accept y
-        }
-        samples.push_back(x);
-    }
-    return samples;
-}
+// Negative Crystal Ball Distribution
+NegativeCrystalBallDistribution::NegativeCrystalBallDistribution(double mean, double sigma, double alpha, double n, double range_min, double range_max, std::string outfile)
+    : FiniteFunction(range_min, range_max, outfile), m_Mean(mean), m_Sigma(sigma), m_Alpha(alpha), m_N(n) {}
 
-// Negative Crystal Ball Implementation
-NegativeCrystalBall::NegativeCrystalBall(double xbar, double sigma, double alpha, double n, double range_min, double range_max, std::string outfile)
-    : FiniteFunction(range_min, range_max, outfile), m_xbar(xbar), m_sigma(sigma), m_alpha(alpha), m_n(n) {}
-
-double NegativeCrystalBall::callFunction(double x) {
-    double A = std::pow(m_n / std::fabs(m_alpha), m_n) * std::exp(-0.5 * m_alpha * m_alpha);
-    double B = m_n / std::fabs(m_alpha) - std::fabs(m_alpha);
-    double C = m_n / std::fabs(m_alpha) * 1.0 / (m_n - 1.0) * std::exp(-0.5 * m_alpha * m_alpha);
-    double D = std::sqrt(M_PI / 2.0) * (1 + std::erf(m_alpha / std::sqrt(2)));
-
-    double N = 1.0 / (m_sigma * (C + D));
-    double t = (x - m_xbar) / m_sigma;
-
-    if (t > -m_alpha) {
-        return N * std::exp(-0.5 * t * t);
+double NegativeCrystalBallDistribution::callFunction(double x) {
+    double t = (x - m_Mean) / m_Sigma;
+    if (t > -m_Alpha) {
+        return exp(-0.5 * t * t);
     } else {
-        return N * A * std::pow(B - t, -m_n);
+        double A = pow(m_N / fabs(m_Alpha), m_N) * exp(-0.5 * m_Alpha * m_Alpha);
+        double B = m_N / fabs(m_Alpha) - fabs(m_Alpha);
+        return A * pow(B - t, -m_N);
     }
 }
 
-void NegativeCrystalBall::printInfo() {
+void NegativeCrystalBallDistribution::printInfo() {
+    std::cout << "Negative Crystal Ball Distribution with mean = " << m_Mean
+              << ", sigma = " << m_Sigma << ", alpha = " << m_Alpha << ", n = " << m_N << std::endl;
     FiniteFunction::printInfo();
-    std::cout << "Negative Crystal Ball Distribution with xbar = " << m_xbar
-              << ", sigma = " << m_sigma << ", alpha = " << m_alpha << ", n = " << m_n << std::endl;
-}
-
-std::vector<double> NegativeCrystalBall::sampleData(int nPoints, double stepSize) {
-    std::vector<double> samples;
-    double x = m_RMin + (m_RMax - m_RMin) * generateRandomUniform(); // Start with a random point
-    for (int i = 0; i < nPoints; ++i) {
-        double y = x + stepSize * (generateRandomUniform() - 0.5); // Propose new point y
-        double A = std::min(callFunction(y) / callFunction(x), 1.0);
-        if (generateRandomUniform() < A) {
-            x = y; // Accept y
-        }
-        samples.push_back(x);
-    }
-    return samples;
 }
--- Exercises2024/Ex3_4/Distributions.h
+++ Exercises2024/Ex3_4/Distributions.h
@@ -1,37 +1,37 @@
 #pragma once
 #include "FiniteFunctions.h"
-#include <cmath>
-#include <vector>
 
-// Normal Distribution Class
 class NormalDistribution : public FiniteFunction {
 public:
-    double m_mu, m_sigma; // Mean and Standard Deviation
-public:
-    NormalDistribution(double mu, double sigma, double range_min, double range_max, std::string outfile);
+    NormalDistribution(double mean, double sigma, double range_min, double range_max, std::string outfile);
     double callFunction(double x) override;
     void printInfo() override;
-    std::vector<double> sampleData(int nPoints, double stepSize); // Metropolis Sampling Method
+
+private:
+    double m_Mean;
+    double m_Sigma;
 };
 
-// Cauchy-Lorentz Distribution Class
 class CauchyLorentzDistribution : public FiniteFunction {
-public:
-    double m_x0, m_gamma; // Location parameter and scale parameter
 public:
     CauchyLorentzDistribution(double x0, double gamma, double range_min, double range_max, std::string outfile);
     double callFunction(double x) override;
     void printInfo() override;
-    std::vector<double> sampleData(int nPoints, double stepSize); // Metropolis Sampling Method
+
+private:
+    double m_X0;
+    double m_Gamma;
 };
 
-// Negative Crystal Ball Distribution Class
-class NegativeCrystalBall : public FiniteFunction {
+class NegativeCrystalBallDistribution : public FiniteFunction {
 public:
-    double m_xbar, m_sigma, m_alpha, m_n; // Parameters for distribution
-public:
-    NegativeCrystalBall(double xbar, double sigma, double alpha, double n, double range_min, double range_max, std::string outfile);
+    NegativeCrystalBallDistribution(double mean, double sigma, double alpha, double n, double range_min, double range_max, std::string outfile);
     double callFunction(double x) override;
     void printInfo() override;
-    std::vector<double> sampleData(int nPoints, double stepSize); // Metropolis Sampling Method
+
+private:
+    double m_Mean;
+    double m_Sigma;
+    double m_Alpha;
+    double m_N;
 };
